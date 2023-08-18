e09c3db87aab43ff2e137567ca1fe90eb66062d8
ChristianAlteri <alteri.christian@gmail.com>
2023-08-18 16:10:54 +1000

    vid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './Assets/exchangelogo.png'
+import React from 'react'; // Import React
+import logo from './Assets/exchangelogo.png';
 import './App.css';
+import ReactPlayer from "react-player";
 
 function App() {
   return (
     <div>
-      <h1>The Grand Exchange <img src={logo} alt='logo' /></h1>
+      <div className="video-container">
+        <h1>The Grand Exchange <img src={logo} alt='logo' /></h1>
+      </div>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=Rtp-Slp58GM"
+        playing={true}
+        volume={0.5}
+      />
     </div>
   );
 }
