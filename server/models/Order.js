bef7d9e93a2e111428beca63fd6e45b9429489d5
mlukicdesign <mlukicdesign@gmail.com>
2023-08-22 21:09:13 +0800

    Created schemas based on ERD

---
+++ server/models/Order.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    purchaseDate: {
+        type: Date,
+        default: Date.now,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    fulfiled: {
+        type: Boolean,
+        default: false,
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+        }
+    ]
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Category = require('./Category');
+const Product = require('./Product');
+const Order = require('./Order');
+
+module.exports = { User, Category, Product, Order };
