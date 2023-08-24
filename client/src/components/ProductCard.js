77665e9e32866b98ccfd8d7ebd2dc0d4a21290dc
ChristianAlteri <alteri.christian@gmail.com>
2023-08-24 13:42:09 +0100

    started building out ui

---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import './index.css'
+// import './App.css'
+
+const ProductCard = () => {
+    return (
+    //   <div className="
+            // flex justify-center items-center h-64 w-64 border border-gray-300 m-4"
+    //                 >
+            <div className="
+            px-4
+            py-3
+            hover:bg-neutral-100
+            transition
+            font-semibold
+        "
+        >
+            Product Card!
+        </div>
+    );
+  };
+
+  export default ProductCard;
