83aca75532c880ad730677a2c490e80052127a4a
ChristianAlteri <alteri.christian@gmail.com>
2023-08-24 08:20:27 +0100

    seed data added

---
+++ server/seeds/seed.js
@@ -0,0 +1,25 @@
+const db = require('../config/connection');
+const { User, Category, Product, Order } = require('../models');
+
+
+const userData = require('./userData.json');
+const categoryData = require('./categoryData.json');
+const productData = require('./productData.json');
+const orderData = require('./orderData.json');
+
+db.once('open', async () => {
+  // clean database
+  await User.deleteMany({});
+  await Category.deleteMany({});
+  await Product.deleteMany({});
+  await Order.deleteMany({});
+
+  // bulk create each model
+  const user = await User.insertMany(userData);
+  const category = await Category.insertMany(categoryData);
+  const product = await Product.insertMany(productData);
+  const order = await Order.insertMany(orderData);
+
+  console.log('all done!');
+  process.exit(0);
+});
