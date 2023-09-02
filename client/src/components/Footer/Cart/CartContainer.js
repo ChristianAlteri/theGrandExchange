ab0bfe0a2789169801f3d3cfc7091ed8e0f8bf20
ChristianAlteri <alteri.christian@gmail.com>
2023-09-02 19:28:57 +0200

    added product total and cart works - need to get logged in user from session because its hard coded rn

--- client/src/components/Footer/Cart/CartContainer.js
+++ client/src/components/Footer/Cart/CartContainer.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import CartItem from '../Cart/CarItem'; // Update the import path if needed
+import React, { useState, useEffect } from "react";
+import CartItem from '../Cart/CarItem'; 
+
 
 const CartContainer = ({ orders }) => {
+
+  const [cartTotal, setCartTotal] = useState(0); 
+
+  // Calculate the total price of items when orders change
+  useEffect(() => {
+    // Initialize a variable to store the total price
+    let total = 0;
+
+    // Loop through orders and their products to calculate the total
+    orders.orders.forEach(order => {
+      order.products.forEach(product => {
+        total += product.price;
+      });
+    });
+
+    // Update the cartTotal state
+    setCartTotal(total);
+  }, [orders]);
+
+  console.log("these are the orders: ", orders.orders);
   return (
     <div className="p-8">
       <div className="flex flex-wrap">
-        {orders.map(order => (
+        {orders.orders.map(order => (
           <div key={order._id}>
             {order.products.map(product => (
               <CartItem key={product._id} product={product} />
             ))}
           </div>
         ))}
+      </div>
+        <div className="border-b">
+        Cart total: ${cartTotal.toFixed(2)} 
       </div>
     </div>
   );
